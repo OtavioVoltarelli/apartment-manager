27108e25673f7b70c5286abbf865361538f67f3d
Otavio Voltarelli <otavio.voltarelli@hotmail.com>
2024-07-16 10:42:33 -0300

    project created

---
+++ src/main/java/com/parking_spot_control/ParkingSpotControlApplication.java
@@ -0,0 +1,13 @@
+package com.parking_spot_control;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingSpotControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingSpotControlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/parking_spot_control/ParkingSpotControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parking_spot_control;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingSpotControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
